247e61dc21b0a009060a02acaad3dab0fb2f7a9e
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-03-21 23:00:10 +0100

    🚨 Fix linter warnings.

--- app/Validators/v1/Private/OrganisationValidator.ts
+++ app/Validators/v1/Private/OrganisationValidator.ts
@@ -5,13 +5,13 @@ export default class OrganisationValidator {
   constructor(private context: HttpContextContract) {}
 
   public schema = schema.create({
-    name: schema.string({trim: true})
+    name: schema.string({trim: true}),
     description: schema.string.optional({escape: true}),
 
     street1: schema.string({trim: true}),
     street2: schema.string.optional({trim: true}),
-    zipCode: schema.string({trim: true})
-    city: schema.string({trim: true})
+    zipCode: schema.string({trim: true}),
+    city: schema.string({trim: true}),
   });
 
   public cacheKey = this.context.routeKey;
