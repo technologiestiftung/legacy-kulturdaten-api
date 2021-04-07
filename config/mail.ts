6ffd9a1a9e91ac35ab4fcdbd9075fcdff3a59760
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-04-07 16:02:29 +0200

    🚨 Format files to match updated ruleset

--- config/mail.ts
+++ config/mail.ts
@@ -5,8 +5,8 @@
  * file.
  */
 
-import Env from '@ioc:Adonis/Core/Env'
-import { MailConfig } from '@ioc:Adonis/Addons/Mail'
+import Env from '@ioc:Adonis/Core/Env';
+import { MailConfig } from '@ioc:Adonis/Addons/Mail';
 
 const mailConfig: MailConfig = {
   /*
@@ -46,14 +46,13 @@ const mailConfig: MailConfig = {
       driver: 'smtp',
       host: Env.get('SMTP_HOST'),
       port: Env.get('SMTP_PORT'),
-			auth: {
-				user: Env.get('SMTP_USERNAME'),
-				pass: Env.get('SMTP_PASSWORD'),
-				type: 'login',
-			}
+      auth: {
+        user: Env.get('SMTP_USERNAME'),
+        pass: Env.get('SMTP_PASSWORD'),
+        type: 'login',
+      },
     },
-
   },
-}
+};
 
-export default mailConfig
+export default mailConfig;
