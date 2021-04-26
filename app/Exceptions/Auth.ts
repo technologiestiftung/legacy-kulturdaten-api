a33932df1a6b5b299aba622de5763e94528a8f41
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-04-26 17:21:51 +0200

    🔧 Force Adonis.js to give JSON:API compliant error responses

--- app/Exceptions/Auth.ts
+++ app/Exceptions/Auth.ts
@@ -1,21 +1,21 @@
 import { Exception } from '@poppinss/utils';
 
-export class InvalidCredentialsException extends Exception {
-  constructor() {
-    super(
-      'The credentials provided can not be used to login',
-      401,
-      'E_INVALID_CREDENTIALS'
-    );
-  }
-}
-
 export class UnauthorizedException extends Exception {
   constructor() {
     super(
       'The action you try to perform is only allowed for authenticated users.',
       401,
       'E_UNAUTHORIZED'
+    );
+  }
+}
+
+export class InvalidCredentialsException extends Exception {
+  constructor() {
+    super(
+      'The credentials provided can not be used to login',
+      401,
+      'E_INVALID_CREDENTIALS'
     );
   }
 }
