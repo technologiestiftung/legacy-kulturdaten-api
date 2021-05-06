45fe3e874f46233e4ad3a322d1f265c43d76f1b9
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-05-06 15:30:05 +0200

    🔒 Mark uid field for organizer as unique

--- database/migrations/1616088075303_organizers.ts
+++ database/migrations/1616088075303_organizers.ts
@@ -6,7 +6,7 @@ export default class Organizers extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id');
-      table.uuid('uid');
+      table.uuid('uid').unique().notNullable();
       table.integer('address_id');
       table.string('name').notNullable();
 
