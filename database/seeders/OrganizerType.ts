c7e9b9dd04a28663e7d3f6e4bc1f8284668fa807
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-07-06 18:00:59 +0200

    🤡 Add updated seeds for organizer type and subjects with seeder

---
+++ database/seeders/OrganizerType.ts
@@ -0,0 +1,34 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import OrganizerType from 'App/Models/OrganizerType';
+import OrganizerSubject from 'App/Models/OrganizerSubject';
+
+import organizerTypeData from '../../seeds/OrganizerType';
+
+export default class OrganizerTypeSeeder extends BaseSeeder {
+  public async run() {
+    const organizerTypes = await Promise.all(
+      organizerTypeData.map(async (resource) => {
+        const organizerType = new OrganizerType();
+        await organizerType.save();
+
+        await organizerType
+          .related('translations')
+          .createMany(resource.relations.translations);
+
+        for (const subjectData of resource.relations.subjects) {
+          const organizerSubject = await organizerType
+            .related('subjects')
+            .create();
+          await organizerSubject
+            .related('translations')
+            .createMany(subjectData.relations.translations);
+
+          await organizerSubject.save();
+        }
+
+        await organizerType.save();
+        return organizerType;
+      })
+    );
+  }
+}
---
+++ seeds/OrganizerType.ts
@@ -0,0 +1,714 @@
+const types = [
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Konzerthäuser, Orchester, Chöre',
+          language: 'de',
+        },
+        {
+          name: 'Conert houses, orchestras, choires',
+          language: 'en',
+        },
+      ],
+      subjects: [
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Konzertbühne',
+                language: 'de',
+              },
+              {
+                name: 'Concert stage',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Sinfonieorchester',
+                language: 'de',
+              },
+              {
+                name: 'Symphonic orchestra',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Kammerorchester',
+                language: 'de',
+              },
+              {
+                name: 'Chamber orchestra',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Spezialensemble',
+                language: 'de',
+              },
+              {
+                name: 'Special ensemble',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Chor',
+                language: 'de',
+              },
+              {
+                name: 'Choir',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Konzertbühne',
+                language: 'de',
+              },
+              {
+                name: 'Concert stage',
+                language: 'en',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Theater, Tanz & Performance',
+          language: 'de',
+        },
+        {
+          name: 'Theatre, dance & performance',
+          language: 'en',
+        },
+      ],
+      subjects: [
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Sprechtheater',
+                language: 'de',
+              },
+              {
+                name: 'Drama',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Musiktheater',
+                language: 'de',
+              },
+              {
+                name: 'Musical',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Tanz',
+                language: 'de',
+              },
+              {
+                name: 'Dance',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Figurentheater',
+                language: 'de',
+              },
+              {
+                name: 'Puppet theatre',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Performance',
+                language: 'de',
+              },
+              {
+                name: 'Performance',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Kleinkunst',
+                language: 'de',
+              },
+              {
+                name: 'Cabaret',
+                language: 'en',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Museum',
+          language: 'de',
+        },
+        {
+          name: 'Museum',
+          language: 'en',
+        },
+      ],
+      subjects: [
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Bildende Kunst',
+                language: 'de',
+              },
+              {
+                name: 'Visual arts',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Architektur, Kunsthandwerk & Design',
+                language: 'de',
+              },
+              {
+                name: 'Architecture, craft & design',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Schlösser & Burgen',
+                language: 'de',
+              },
+              {
+                name: 'Castles',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Naturkunde',
+                language: 'de',
+              },
+              {
+                name: 'Natural History',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Naturwissenschaft & Technik',
+                language: 'de',
+              },
+              {
+                name: 'Science & Technology',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Geschichte & Archäologie',
+                language: 'de',
+              },
+              {
+                name: 'History & Archaeology',
+                language: 'en',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Gedenkstätte',
+          language: 'de',
+        },
+        {
+          name: 'Memorial',
+          language: 'en',
+        },
+      ],
+      subjects: [],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Bildungseinrichtung',
+          language: 'de',
+        },
+        {
+          name: 'Educational',
+          language: 'en',
+        },
+      ],
+      subjects: [
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Musikschule',
+                language: 'de',
+              },
+              {
+                name: 'School for Music',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Theaterschule',
+                language: 'de',
+              },
+              {
+                name: 'Theatre School',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Schauspielschule',
+                language: 'de',
+              },
+              {
+                name: 'Acting School',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Tanzschule',
+                language: 'de',
+              },
+              {
+                name: 'Dancing school',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Kulturelle Bildung',
+                language: 'de',
+              },
+              {
+                name: 'Cultural education',
+                language: 'en',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Bibliotheken & Archive',
+          language: 'de',
+        },
+        {
+          name: 'Libraries & Archives',
+          language: 'en',
+        },
+      ],
+      subjects: [
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Universitätsbibliothek',
+                language: 'de',
+              },
+              {
+                name: 'University Library',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Universalbibliothek',
+                language: 'de',
+              },
+              {
+                name: 'Universal Library',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Fachbibliothek',
+                language: 'de',
+              },
+              {
+                name: 'Fachbibliothek',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Archiv',
+                language: 'de',
+              },
+              {
+                name: 'Archive',
+                language: 'en',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Club',
+          language: 'de',
+        },
+        {
+          name: 'Club',
+          language: 'en',
+        },
+      ],
+      subjects: [],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Literaturhaus',
+          language: 'de',
+        },
+        {
+          name: 'House of Literature',
+          language: 'en',
+        },
+      ],
+      subjects: [],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Projektraum',
+          language: 'de',
+        },
+        {
+          name: 'Project Space',
+          language: 'en',
+        },
+      ],
+      subjects: [],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Freie Künstler:in, Gruppe oder Kollektiv',
+          language: 'de',
+        },
+        {
+          name: 'Artists',
+          language: 'en',
+        },
+      ],
+      subjects: [
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Musik & Gesang',
+                language: 'de',
+              },
+              {
+                name: 'Music & Singing',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Tanz',
+                language: 'de',
+              },
+              {
+                name: 'Dance',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Theater & Schauspiel',
+                language: 'de',
+              },
+              {
+                name: 'Theatre & Acting',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Performance',
+                language: 'de',
+              },
+              {
+                name: 'Performance',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Kunsthandwerk',
+                language: 'de',
+              },
+              {
+                name: 'Arts and crafts',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Medienkunst',
+                language: 'de',
+              },
+              {
+                name: 'Media Arts',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Literatur',
+                language: 'de',
+              },
+              {
+                name: 'Literature',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Bildende Kunst',
+                language: 'de',
+              },
+              {
+                name: 'Visual Arts',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Fotografie & Film',
+                language: 'de',
+              },
+              {
+                name: 'Photography & Filming',
+                language: 'en',
+              },
+            ],
+          },
+        },
+        {
+          relations: {
+            translations: [
+              {
+                name: 'Kulturelle Bildung',
+                language: 'de',
+              },
+              {
+                name: 'Cultural education',
+                language: 'en',
+              },
+            ],
+          },
+        },
+      ],
+    },
+  },
+
+  {
+    attributes: {},
+    relations: {
+      translations: [
+        {
+          name: 'Sonstige',
+          language: 'de',
+        },
+        {
+          name: 'Other',
+          language: 'en',
+        },
+      ],
+      subjects: [],
+    },
+  },
+];
+
+export default types;
