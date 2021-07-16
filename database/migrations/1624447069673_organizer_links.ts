8d0f4bb9a45e74b4108cd70747235e55c4f3a0d4
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-07-16 20:11:58 +0200

    🎨 Set correct class name

--- database/migrations/1624447069673_organizer_links.ts
+++ database/migrations/1624447069673_organizer_links.ts
@@ -1,6 +1,6 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema';
 
-export default class OrganizerOrganizerSubjects extends BaseSchema {
+export default class OrganizerLinks extends BaseSchema {
   protected tableName = 'organizer_links';
 
   public async up() {
