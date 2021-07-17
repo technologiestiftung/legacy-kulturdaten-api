7efb5d6aa57126685312b1afeebbe0f6769d1596
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-07-17 10:12:08 +0200

    ♻️ Make superuser a flag on the user instead of a seperate table

--- database/migrations/1587988332388_users.ts
+++ database/migrations/1587988332388_users.ts
@@ -10,6 +10,7 @@ export default class UsersSchema extends BaseSchema {
       table.string('email', 255).notNullable();
       table.string('password', 180).notNullable();
       table.string('remember_me_token').nullable();
+      table.boolean('is_superuser');
       table
         .enu('status', [UserStatus.ACTIVE, UserStatus.INACTIVE])
         .defaultTo(UserStatus.INACTIVE);
