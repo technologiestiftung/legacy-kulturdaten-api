05687001c88ce55b84c9f0a0cde71f635c9c63f8
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-08-23 19:33:33 +0200

    🎨 Clean images when resetting database

--- commands/bootstrap.ts
+++ commands/bootstrap.ts
@@ -4,6 +4,11 @@ import Encryption from '@ioc:Adonis/Core/Encryption';
 import databaseConfig from 'Config/database';
 import { unlink } from 'fs/promises';
 import execa from 'execa';
+import del from 'del';
+import { join } from 'path';
+import Application from '@ioc:Adonis/Core/Application';
+
+const IMAGES_BASE_PATH = Application.publicPath('media/images');
 
 export default class Bootstrap extends BaseCommand {
   public static commandName = 'bootstrap';
@@ -31,6 +36,19 @@ export default class Bootstrap extends BaseCommand {
 
   public async run() {
     if (this.reset) {
+      try {
+        await del([
+          join(IMAGES_BASE_PATH, 'original', '**/*'),
+          join(`!${IMAGES_BASE_PATH}`, 'original', '.gitkeep'),
+          join(IMAGES_BASE_PATH, 'renditions', '**/*'),
+          join(`!${IMAGES_BASE_PATH}`, 'renditions', '.gitkeep'),
+        ]);
+        this.logger.success(`Cleaned media files.`);
+      } catch (e) {
+        this.logger.error(`There was an error cleaning the media files.`);
+        this.logger.error(e);
+      }
+
       if (databaseConfig.connection !== 'sqlite') {
         this.logger.error(
           `Resetting the database only works for SQLite databases.`
