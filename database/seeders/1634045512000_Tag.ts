a998fbc9dbbc0a7f6fccdb135e16e03dd81dba0e
Boris Fruendt <hey@boris.io>
2021-10-12 15:45:53 +0200

    🤡 Add seeds for tags with seeder

---
+++ database/seeders/1634045512000_Tag.ts
@@ -0,0 +1,22 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder';
+import Tag from 'App/Models/Tag';
+
+import tagData from '../../seeds/Tag';
+
+export default class TagSeeder extends BaseSeeder {
+  public async run() {
+    const tag = await Promise.all(
+      tagData.map(async (resource) => {
+        const tag = new Tag();
+        await tag.save();
+
+        await tag
+          .related('translations')
+          .createMany(resource.relations.translations);
+
+        await tag.save();
+        return tag;
+      })
+    );
+  }
+}
