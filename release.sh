4f37b10c0a89b7d15a11228a0dd7524d08079c4f
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-10-13 11:46:53 +0200

    🔧 Reenable release task for beta as well

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 echo "Starting release ..."
-if [ $NODE_ENV == "alpha" ]; then
+if [ $NODE_ENV == "beta" ]; then
   echo "Rolling back migrations ..."
   node ace migration:rollback --force --batch 0
 fi
@@ -7,7 +7,7 @@ fi
 echo "Running migrations ..."
 node ace migration:run
 
-if [ $NODE_ENV == "alpha" ]; then
+if [ $NODE_ENV == "beta" ]; then
   echo "Loading fixtures ..."
   node ace fixtures:load --verbose
   echo "Seeding database ..."
