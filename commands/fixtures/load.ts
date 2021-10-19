ecc0e35a421debe9bc079605ecc849b2c12abcf7
Matthias Rohmer <rohmer.matthias@anyvent.org>
2021-10-19 11:13:16 +0200

    🔇 Silence logs in fixtures loader

--- commands/fixtures/load.ts
+++ commands/fixtures/load.ts
@@ -101,7 +101,6 @@ export default class LoadFixtures extends BaseCommand {
 
   public async run() {
     this.fixturePaths = await this.listFixtureFiles();
-    console.log(this.fixturePaths);
 
     for (const path of this.fixturePaths) {
       const fixture = await this.loadFixture(path);
