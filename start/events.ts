cd6c0a38b93a314c1c100e1924856ecbe6d13c82
Matthias Rohmer <rohmer.matthias@anyvent.org>
2022-02-15 18:29:22 +0100

    🐛 Use correct casing for adressing listener classes

--- start/events.ts
+++ start/events.ts
@@ -3,10 +3,10 @@ import Logger from '@ioc:Adonis/Core/Logger';
 import Database from '@ioc:Adonis/Lucid/Database';
 import Application from '@ioc:Adonis/Core/Application';
 
-Event.on('user:new', 'user.sendVerificationMail');
+Event.on('user:new', 'User.sendVerificationMail');
 
-Event.on('auth:requestPasswordReset', 'auth.sendPasswordResetInstructions');
-Event.on('auth:passwordReset', 'auth.sendPasswortResetNotification');
+Event.on('auth:requestPasswordReset', 'Auth.sendPasswordResetInstructions');
+Event.on('auth:passwordReset', 'Auth.sendPasswortResetNotification');
 
 Event.on('new:invitation', 'NewInvitation.sendInvitationMail');
 
