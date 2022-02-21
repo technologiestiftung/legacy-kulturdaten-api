48179dbe2cc1e725aabda6ce9668ad9bbdedbf55
Matthias Rohmer <rohmer.matthias@anyvent.org>
2022-02-21 15:49:59 +0100

    🗃 Use text data type for url column to be able to store S3 urls

--- database/migrations/1616088075302_renditions.ts
+++ database/migrations/1616088075302_renditions.ts
@@ -7,7 +7,7 @@ export default class Renditions extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id');
 
-      table.string('url');
+      table.text('url');
       table.integer('base');
       table.integer('width');
       table.integer('height');
